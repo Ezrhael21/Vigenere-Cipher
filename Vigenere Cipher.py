aa41f65cada45b657f972de26cbbbdd5a4185a6e
Ezrhael21 <ezrhaelsicat21@gmail.com>
2023-04-17 13:34:19 +0800

    Pseudocode

---
+++ Vigenere Cipher.py
@@ -0,0 +1,16 @@
+# Ezrhael R. Sicat
+# BSCpE 1-5
+# 04/05/2023
+# Program 3 - The Vigenere Cipher
+
+# Ask the user for the message
+# Ask the user for key
+# Create a list of letters
+# Create a list of numbers from 0 to 25
+# Convert the length of the key to the same length of the message
+# Encrypt the user text to its corresponding number
+# Encrypt the user key to its corresponding number
+# Add the numbers of the message and the key
+# Modify the numbers into the list
+# Convert the modified number to its corresponding letter
+# Print Output
